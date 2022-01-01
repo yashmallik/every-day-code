11151dc63139592f87cfcb480cbafbd95f43687c
yashmallik <76205209+yashmallik@users.noreply.github.com>
2022-01-02 02:06:16 +0530

    Update 1_two sum.js

--- javascript/1_two sum.js
+++ javascript/1_two sum.js
@@ -24,3 +24,20 @@ var twoSum = function(nums, target) {
     return pos
 
 };
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let obj = {}
+    for(let i = 0; i<nums.length; i++){
+        let comp = target-nums[i];
+        if(obj.hasOwnProperty(comp)){
+            return[obj[comp],i]
+        }
+        obj[nums[i]] = i;
+    }
+};
