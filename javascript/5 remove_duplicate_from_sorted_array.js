6ac42a7c6383aebc963294c9b84ec35797a59c7e
yashmallik <76205209+yashmallik@users.noreply.github.com>
2022-01-06 04:09:00 +0530

    Update 5 remove_duplicate_from_sorted_array.js

--- javascript/5 remove_duplicate_from_sorted_array.js
+++ javascript/5 remove_duplicate_from_sorted_array.js
@@ -6,4 +6,17 @@
 
 // Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
+
 ____________________________________________________________________________________________________________________________________________________________________________
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+for(i = 0; i<nums.length-1;i++){
+    if(nums[i]==nums[i+1]){
+        nums.splice(i,1);
+        i--;
+    }
+}return nums.length;    
+};
